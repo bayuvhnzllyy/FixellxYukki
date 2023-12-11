0c4f8a102c944a3c59cfdbc32d50ed3a72b08cc1
bayuvhnzllyy <151582580+bayuvhnzllyy@users.noreply.github.com>
2023-12-12 04:48:00 +0700

    Update play.py

--- YukkiMusic/utils/inline/play.py
+++ YukkiMusic/utils/inline/play.py
@@ -65,7 +65,7 @@ def stream_markup(_, videoid):
         [
      
             InlineKeyboardButton(
-                text=_["S_B_9"], url=f"https://t.me/mahadappa",
+                text=_["S_B_9"], url=f"https://t.me/exstorehere",
             ),
 
 
